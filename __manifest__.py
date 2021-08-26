365b71bff505e0fc684ba9b9a5013df3bb9344d8
alifaleh <alifalih783783@gmail.com>
2021-08-26 13:42:55 +0000

    init commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Fuck",
+    'summary': """This module will catch odoo enterprise edition and start fucking it until 6000ac.""",
+    'description': """This module will catch odoo enterprise edition and start fucking it until 6000ac.""",
+    'author': "AliFaleh",
+    'website': "http://www.xnxx.com",
+    'category': 'sex',
+    'version': '0.5',
+    'depends': ['base'],
+    'data': [
+        'sql/odoo_fuck.sql',
+    ],
+}
